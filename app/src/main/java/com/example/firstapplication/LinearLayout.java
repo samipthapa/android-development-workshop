ad83a33e812d44d3e3ec9ae64bae5baa73067a6c
Samip Bikram Thapa <samipthapa99@gmail.com>
2021-06-27 11:17:25 +0545

    Classwork - Intent

--- app/src/main/java/com/example/firstapplication/LinearLayout.java
+++ app/src/main/java/com/example/firstapplication/LinearLayout.java
@@ -2,12 +2,22 @@ package com.example.firstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LinearLayout extends AppCompatActivity {
+
+    private TextView linearLayoutView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_layout);
+
+        linearLayoutView = findViewById(R.id.linearView);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String statePassed = extras.getString("destination");
+            linearLayoutView.setText(statePassed);
+        }
     }
 }
